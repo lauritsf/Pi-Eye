9449b902fea2a20a3dba2a66a96b8aa8ef8250d7
Laurits Fredsgaard Larsen <laurits@fredsgaardlarsen.com>
2023-08-08 13:52:00 +0200

    chore: Update install, uninstall, and update scripts
    Updated `install_pieye.sh` to include the option of passing a custom repository URL as the first argument. Additionally, added error checks to ensure that Git and Pip are installed on the system. The script now checks for the existence of the repository directory before cloning and installing the Pieye camera software. It enables the watchdog and sets up auto-login on the command line. Moreover, it copies `display_info.sh` to the `/home/pi` folder and adds an entry to execute `display_info.sh` on login.
    
    In the `uninstall_pieye.sh` script, removed `display_info.sh` from the `/home/pi` folder and eliminated the corresponding entry from the `.profile` file.
    
    In the `update_pieye.sh` script, updated the logic to check for local changes before updating the Pieye camera software, preventing updates when uncommitted changes exist. Error handling ensures that the Pieye service starts correctly after the update.

--- scripts/install_pieye.sh
+++ scripts/install_pieye.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
+set -e
 
-REPO_URL="https://github.com/NHMDenmark/Pi-Eye.git"
+DEFAULT_REPO_URL="https://github.com/NHMDenmark/Pi-Eye.git"
+
+REPO_URL=${1:-$DEFAULT_REPO_URL} # Allow passing in a custom repository URL as first argument
+REPO_DIR="/home/pi/Pi-Eye"
+
+# Ensure git and pip are installed
+command -v git >/dev/null 2>&1 || { echo "git is not installed. Exiting."; exit 1; }
+command -v pip >/dev/null 2>&1 || { echo "pip is not installed. Exiting."; exit 1; }
+
+# Check if repository directory exists
+if [ -d "$REPO_DIR" ]; then
+    echo "Directory $REPO_DIR already exists. Exiting."
+    exit 1
+fi
 
 # Clone repository
-git clone "$REPO_URL" /home/pi/Pi-Eye
+git clone "$REPO_URL" "$REPO_DIR"
 
 # Change directory
-cd /home/pi/Pi-Eye
+cd "$REPO_DIR"
 
 # Install the Python package
 pip install .
@@ -17,10 +31,20 @@ sudo cp pieye.service /lib/systemd/system/
 # Enable and start the service
 sudo systemctl enable pieye.service
 sudo systemctl start pieye.service
+sudo systemctl is-active pieye.service || { echo "Failed to start pieye.service. Exiting."; exit 1; }
 
 # Turn on watchdog
 sudo sed -i 's/#RuntimeWatchdogSec=0/RuntimeWatchdogSec=10/' /etc/systemd/system.conf
 sudo sed -i 's/#ShutdownWatchdogSec=10min/ShutdownWatchdogSec=2min/' /etc/systemd/system.conf
+
+# Setup autologin (cli)
+sudo raspi-config nonint do_boot_behaviour B2
+
+# Copy display_info.sh to /home/pi folder
+cp scripts/display_info.sh /home/pi
+
+# Make display_info.sh execute on login
+echo "bash /home/pi/display_info.sh" >> /home/pi/.profile
 
 # Get version of package
 VERSION=$(python -c "import pieye; print(pieye.__version__)")
